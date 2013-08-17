516e7591b6ecbc545f24984fd834a273f895101a
Rob Beagrie <rob@beagrie.com>
2013-08-17 10:17:45 +0100

    Trying to get a default cms install still, I really borked this repo...

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(r'^', include('cms.urls')),
 
     # Examples:
     # url(r'^$', '{{ project_name }}.views.home', name='home'),
